a38f4bbb1d17ea3cf8e74afbed9292cc90fe28ac
JoelSerinLee <93472196+JoelSerinLee@users.noreply.github.com>
2021-10-31 20:27:51 +0900

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpeg.JPG", "1.jpeg.JPG", "2.jpeg.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
